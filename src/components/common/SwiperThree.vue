da91c6da9a4d10a0caf7ccb7007fd95d7a27d308
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-16 19:54:09 +0800

    chaneg

---
+++ src/components/common/SwiperThree.vue
@@ -0,0 +1,4 @@
+<template>
+
+    11
+</template>
--- src/views/Gal/GalAugust.vue
+++ src/views/Gal/GalAugust.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>GalAugust</h1>
+    <section class="container">
+        <SwiperThree />
+    </section>
+    
 </template>
+
+
+<script setup>
+import SwiperThree from '@/components/common/SwiperThree.vue';
+
+
+
+
+</script>
+
+
+<style >
+.container{
+    width: 100%;
+    height: 700px;
+
+
+}
+
+
+
+
+</style>
