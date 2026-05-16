b39b6f83a22f8984c17792e92cb4e7e234a60601
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-16 10:35:15 +0800

    galpiniastrore

---
+++ src/stores/useGalGameStore.js
@@ -0,0 +1,33 @@
+//GAL组件categories=10里面的图片抓取
+
+import { defineStore } from 'pinia'
+import axios from 'axios' // 
+
+export const useGalGameStore = defineStore('work', {
+  state: () => ({
+    GalImgData: [], 
+    isLoading: false
+  }),
+
+  actions: {
+    // 定义一个抓取数据的方法
+    async fetchGalImgData(categories=10) {
+      this.isLoading = true
+      try {
+        // 请求你的 WordPress 接口，记得带上 _embed 才能拿到图片
+        const response = await axios.get(`http://110.42.248.8:8080/wp-json/wp/v2/posts`, {
+          params: {
+            categories:categories,
+            _embed: true 
+          }
+        })
+        // 把抓到的数组赋值给 state
+        this.swiperData = response.data
+      } catch (error) {
+        console.error('抓取失败:', error)
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+})
