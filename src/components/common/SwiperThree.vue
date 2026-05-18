65048b9842715dec202eb836894079fd497a3b38
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-18 16:17:22 +0800

    change

--- src/components/common/SwiperThree.vue
+++ src/components/common/SwiperThree.vue
@@ -1,55 +1,150 @@
 <template>
-  <swiper class="mySwiper">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  <swiper
+    :modules="modules"
+    :slides-per-view="slidesPerView"
+    :space-between="spaceBetween"
+    :effect="effect"
+    :autoplay="autoplayEnabled ? autoplayConfig : false"
+    :loop="loop"
+    :pagination="paginationEnabled ? { clickable: true } : false"
+    :navigation="navigationEnabled"
+    class="swiper-three"
+    @swiper="onSwiperInit"
+    @slide-change="onSlideChange"
+  >
+    <swiper-slide v-for="(item, index) in images" :key="index">
+      <div class="slide-content">
+        <img :src="item.img" :alt="item.alt || 'slide ' + (index + 1)" />
+        <div v-if="item.title || item.desc" class="slide-caption">
+          <h3 v-if="item.title">{{ item.title }}</h3>
+          <p v-if="item.desc">{{ item.desc }}</p>
+        </div>
+      </div>
+    </swiper-slide>
   </swiper>
 </template>
-<script >
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-  // Import Swiper styles
-  import 'swiper/css';
+<script setup>
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, EffectFade, EffectCoverflow, Pagination, Navigation } from 'swiper/modules'
 
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
-  export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      return {};
-    },
-  };
+const props = defineProps({
+  images: {
+    type: Array,
+    required: true,
+    default: () => []
+  },
+  effect: {
+    type: String,
+    default: 'slide'
+  },
+  slidesPerView: {
+    type: Number,
+    default: 1
+  },
+  spaceBetween: {
+    type: Number,
+    default: 0
+  },
+  autoplay: {
+    type: [Boolean, Object],
+    default: true
+  },
+  loop: {
+    type: Boolean,
+    default: true
+  },
+  pagination: {
+    type: Boolean,
+    default: false
+  },
+  navigation: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits(['swiper-init', 'slide-change'])
+
+const modules = [Autoplay, EffectFade, EffectCoverflow, Pagination, Navigation]
+
+const autoplayEnabled = ref(props.autoplay !== false)
+const autoplayConfig = ref(
+  typeof props.autoplay === 'object'
+    ? props.autoplay
+    : { delay: 3000, disableOnInteraction: false }
+)
+const paginationEnabled = ref(props.pagination)
+const navigationEnabled = ref(props.navigation)
+
+const swiperInstance = ref(null)
+
+const onSwiperInit = (swiper) => {
+  swiperInstance.value = swiper
+  emit('swiper-init', swiper)
+}
+
+const onSlideChange = (swiper) => {
+  emit('slide-change', {
+    swiper,
+    activeIndex: swiper.activeIndex,
+    realIndex: swiper.realIndex
+  })
+}
+
+defineExpose({
+  swiper: swiperInstance,
+  slideNext: () => swiperInstance.value?.slideNext(),
+  slidePrev: () => swiperInstance.value?.slidePrev(),
+  slideTo: (index) => swiperInstance.value?.slideTo(index)
+})
 </script>
 
-
 <style scoped>
-
-.swiper {
+.swiper-three {
   width: 100%;
   height: 100%;
 }
 
-.swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: #444;
-
-  /* Center slide text vertically */
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.slide-content {
+  width: 100%;
+  height: 100%;
+  position: relative;
 }
 
-.swiper-slide img {
+.slide-content img {
   display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
 
+.slide-caption {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 24px 20px;
+  background: linear-gradient(transparent, rgba(0, 0, 0, 0.65));
+  color: #fff;
+}
+
+.slide-caption h3 {
+  font-size: 1.125rem;
+  font-weight: 600;
+  margin: 0 0 4px 0;
+}
+
+.slide-caption p {
+  font-size: 0.875rem;
+  margin: 0;
+  opacity: 0.85;
+}
 </style>
--- src/views/Gal/GalAugust/GalAugust.vue
+++ src/views/Gal/GalAugust/GalAugust.vue
@@ -1,16 +1,19 @@
 <template>
     <section class="container">
-        <SwiperThree />
+        <SwiperThree :images="images" :autoplay="{ delay: 3000 }" :loop="true" />
     </section>
-    
 </template>
 
-
 <script setup>
-import SwiperThree from '@/components/common/SwiperThree.vue';
+import SwiperThree from '@/components/common/SwiperThree.vue'
 
-
-
+const images = [
+  { img: 'https://picsum.photos/id/1015/900/500', alt: '风景1', title: '山间溪流', desc: 'August 社作品' },
+  { img: 'https://picsum.photos/id/1016/900/500', alt: '风景2', title: '雪山远景', desc: 'August 社作品' },
+  { img: 'https://picsum.photos/id/1018/900/500', alt: '风景3', title: '湖边山色', desc: 'August 社作品' },
+  { img: 'https://picsum.photos/id/1020/900/500', alt: '风景4', title: '林间小径', desc: 'August 社作品' },
+  { img: 'https://picsum.photos/id/1025/900/500', alt: '风景5', title: '秋日森林', desc: 'August 社作品' },
+]
 </script>
 
 
