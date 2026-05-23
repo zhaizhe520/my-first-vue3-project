5874f7efeeb5bd8a6a5a68c2ec4fdd98e4558157
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-23 20:23:47 +0800

    Anime写字

--- src/views/Anime/Anime.vue
+++ src/views/Anime/Anime.vue
@@ -1,7 +1,25 @@
 <template>
+    <div class="container">
+        <h1>
+            没有时间写数据库写接口！！！！！！！！！！！！！！！
+        </h1>
+    </div>
+
 
 </template>
 <script setup>
 
 
 </script>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: pink;
+
+}
+
+</style>
