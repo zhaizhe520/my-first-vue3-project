8c7f8a235b2c184a29c32b9c3ae749382b46efd7
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-27 17:06:32 +0800

    sever

--- src/views/Gal/GalLumpOfSugar/GalLumpOfSugar.vue
+++ src/views/Gal/GalLumpOfSugar/GalLumpOfSugar.vue
@@ -1,3 +1,31 @@
 <template>
-    12
+<div>
+    <h2>useGalgameStore</h2>
+    <p v-if="store.isLoading">加载中</p>
+    <ul v-else>
+        <li v-for="w in store.works" :key="w.work_id">
+            {{ w.title }} — {{ w.company?.name || '未知公司' }}
+        </li>
+    </ul>
+</div>
+
 </template>
+
+
+<script setup>
+import{ onMounted } from 'vue'
+import { useGalgameStore } from '@/stores/galgameStore'
+
+const store = useGalgameStore()
+
+onMounted ( ()=>{
+    store.fetchWorks();
+})
+</script>
+
+<style scoped>
+
+
+</style>
+
+
