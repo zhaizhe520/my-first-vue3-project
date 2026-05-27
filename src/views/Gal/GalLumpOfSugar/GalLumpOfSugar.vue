d6d842e06b5ed1906cc8ecf36291879f99c937bd
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-27 18:01:32 +0800

    server

--- src/views/Gal/GalLumpOfSugar/GalLumpOfSugar.vue
+++ src/views/Gal/GalLumpOfSugar/GalLumpOfSugar.vue
@@ -3,8 +3,8 @@
     <h2>useGalgameStore</h2>
     <p v-if="store.isLoading">加载中</p>
     <ul v-else>
-        <li v-for="w in store.works" :key="w.work_id">
-            {{ w.title }} — {{ w.company?.name || '未知公司' }}
+        <li v-for="w in store.works" :key="w.id">
+            {{ w.title }}{{ w.id }}
         </li>
     </ul>
 </div>
